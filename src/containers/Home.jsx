df952424a9ce23d06b94d52eb067b06aab69594e
yannyyss <GitYan12>
2019-03-24 19:17:11 -0600

    component table adde to home view

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import TableCountries from "../components/table/TableCountries";
 
 const Home = () => {
   return (
     <div>
       <h2>Hello! Start your search here</h2>
+      <TableCountries />
     </div>
   );
 };
