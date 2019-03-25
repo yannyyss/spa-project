daa685f9d8f4e516770afd51ab9477df333d1d1d
yannyyss <GitYan12>
2019-03-24 19:15:14 -0600

    Table component added

--- src/components/table/TableCountries.jsx
+++ src/components/table/TableCountries.jsx
@@ -1,16 +1,57 @@
-import React from "react";
-import { Table, TableBody, TableCell, TableHead } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow
+} from "@material-ui/core";
+import axios from "axios";
 
 const TableCountries = () => {
+  const [dataState, setDataState] = useState(0);
+
+  useEffect(() => {
+    const config = {
+      headers: {
+        "Access-Control-Allow-Origin": true
+      }
+    };
+
+    axios
+      .get("https://date.nager.at/Api",{
+        headers: config
+      })
+
+      .then(res => {
+        const data = res.data;
+        console.log(data);
+        setDataState({ data });
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }, [dataState]);
+
   return (
     <div>
-      <h2>Hello! Start your search here</h2>
       <Table>
         <TableHead>
-          <TableCell />
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell>Event</TableCell>
+          </TableRow>
         </TableHead>
         <TableBody>
-          <TableCell />
+          {
+            //dataState.data.map((detail, index) => {
+            //return (
+            <TableRow>
+              <TableCell>T</TableCell>
+            </TableRow>
+            //);
+            //})
+          }
         </TableBody>
       </Table>
     </div>
