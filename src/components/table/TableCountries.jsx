46cb61d94420691b866591365e30e76a67be1d0b
yannyyss <GitYan12>
2019-03-28 07:12:37 -0600

    First dropdown working with table

--- src/components/table/TableCountries.jsx
+++ src/components/table/TableCountries.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
 import {
+  FormControl,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  Select,
   Table,
   TableBody,
   TableCell,
@@ -7,34 +12,74 @@ import {
   TableRow
 } from "@material-ui/core";
 import axios from "axios";
+import jsonData from "../../data/data.json";
+import countriesData from "../../data/countries.json";
 
 const TableCountries = () => {
-  const [dataState, setDataState] = useState(0);
+  const [dataState, setDataState] = useState({});
+  const [countriesState, setCountriesState] = useState({});
+  const [queryCountryCode, setQueryCountryCode] = useState("");
 
   useEffect(() => {
-    const config = {
-      headers: {
-        "Access-Control-Allow-Origin": true
-      }
-    };
+    if (queryCountryCode !== "") {
+      axios
+        .post(`http://localhost:3000/api/holidays`, {
+          countryCode: queryCountryCode
+        })
+
+        .then(res => {
+          const data = res.data;
+          console.log("holidays from back:", data);
+          setDataState({ data });
+        })
+        .catch(e => {
+          console.log(e);
+          const data = jsonData;
+          setDataState({ data });
+        });
+    }
 
     axios
-      .get("https://date.nager.at/Api",{
-        headers: config
-      })
-
-      .then(res => {
-        const data = res.data;
-        console.log(data);
-        setDataState({ data });
+      .get("http://localhost:3000/api/all")
+      .then(response => {
+        console.log("info:", response.data);
+        const res = response.data;
+        setCountriesState({ res: res });
       })
       .catch(e => {
         console.log(e);
+        //setCountriesState({ countries });
       });
-  }, [dataState]);
+  }, [queryCountryCode]);
 
+  const getCountryQuery = e => {
+    const value = e.target.value;
+    console.log("key:", value);
+    setQueryCountryCode(value);
+  };
   return (
     <div>
+      {countriesState.res
+        ? console.log("countries :)" + countriesState.res)
+        : console.log("countries :(" + countriesState.res)}
+      {dataState.data
+        ? console.log("data :)" + dataState.data)
+        : console.log("data :(" + dataState.data)}
+      <FormControl>
+        <InputLabel>Countries</InputLabel>
+        {countriesState.res ? (
+          <Select value={countriesState.res} onChange={getCountryQuery}>
+            {countriesState.res.map((detail, index) => {
+              return (
+                <MenuItem key={index} value={detail.key}>
+                  <ListItemText primary={detail.key} value={detail.key} />
+                </MenuItem>
+              );
+            })}
+          </Select>
+        ) : null}
+      </FormControl>
+
       <Table>
         <TableHead>
           <TableRow>
@@ -43,15 +88,16 @@ const TableCountries = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {
-            //dataState.data.map((detail, index) => {
-            //return (
-            <TableRow>
-              <TableCell>T</TableCell>
-            </TableRow>
-            //);
-            //})
-          }
+          {dataState.data
+            ? dataState.data.map((detail, index) => {
+                return (
+                  <TableRow>
+                    <TableCell>{detail.date}</TableCell>
+                    <TableCell>{detail.name}</TableCell>
+                  </TableRow>
+                );
+              })
+            : null}
         </TableBody>
       </Table>
     </div>
